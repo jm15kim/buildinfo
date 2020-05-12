c6b7ec2af16fb1d31d822c3db8e80a830f8048f2
Jungmin Kim <jm15.kim@samsung.com>
2020-05-12 17:00:00 +0900

    Updates

---
+++ precache-manifest.ef79b27d0466d255e1988b1fbf9e63a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f096d78a128277e2446328387272a52d",
+    "url": "/buildinfo/index.html"
+  },
+  {
+    "revision": "b6b6f5e7d6e9dca47a5b",
+    "url": "/buildinfo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd25bf603745ea560b61",
+    "url": "/buildinfo/static/js/2.c71f1797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buildinfo/static/js/2.c71f1797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b6f5e7d6e9dca47a5b",
+    "url": "/buildinfo/static/js/main.04df2620.chunk.js"
+  },
+  {
+    "revision": "de61dadf69440019d0a4",
+    "url": "/buildinfo/static/js/runtime-main.ecf72b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buildinfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jm15kim/buildinfo/precache-manifest.3ddda31f09be983457ec65cd7bcc1106.js"
+  "/buildinfo/precache-manifest.ef79b27d0466d255e1988b1fbf9e63a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jm15kim/buildinfo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buildinfo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
